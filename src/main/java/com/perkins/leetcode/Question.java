8049761a9fca9f9a8e58ff58d7a6d966f26c3f69
PerkinsZhu <perkinszhu@sina.cn>
2023-07-13 14:47:36 +0800

    question12 //todo

---
+++ src/main/java/com/perkins/leetcode/Question.java
@@ -0,0 +1,12 @@
+package com.perkins.leetcode;
+
+/**
+ * @author: perkins Zhu
+ * @date: 2023/7/13 10:36
+ * @description:
+ **/
+public class Question {
+    public static void main(String[] args) {
+        new Question();
+    }
+}
---
+++ src/main/java/com/perkins/leetcode/Question12.java
@@ -0,0 +1,32 @@
+package com.perkins.leetcode;
+
+/**
+ * @author: perkins Zhu
+ * @date: 2023/7/13 10:36
+ * @description: 字符          数值
+ * I             1
+ * V             5
+ * X             10
+ * L             50
+ * C             100
+ * D             500
+ * M             1000
+ * 4  9
+ * 40 90
+ * 400 900
+ * 1 <= num <= 3999
+ * <p>
+ * <p>
+ * MMM CM XC IX
+ **/
+public class Question12 {
+    public String intToRoman(int num) {
+        String res = "";
+        //TODO
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Question12().intToRoman(60));
+    }
+}
